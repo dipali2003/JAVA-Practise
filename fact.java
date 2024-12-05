634a7a0268b0dbfd472869562137beb9ca471bfb
Dipali lavate <120242897+dipali2003@users.noreply.github.com>
2024-12-05 15:29:44 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,13 @@
+class fact
+{ 
+   public static void main(String agrs[])
+   {
+     int no=6;
+     int fact=1;
+     for(int i=no;i>=1;i--)
+      { 
+        fact=fact*i;
+      }
+       System.out.println(fact);
+   }
+}
