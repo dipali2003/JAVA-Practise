e75d5dc68389035ef2b4e2580b40562ed642cc2f
Dipali lavate <120242897+dipali2003@users.noreply.github.com>
2024-12-05 15:25:54 +0530

    Add files via upload

---
+++ arraymarks.java
@@ -0,0 +1,15 @@
+class arraymarks{
+  public static void main(String args[])
+  {
+     int[]marks=new int[5];
+     marks[0]=95;
+     marks[1]=99;
+     marks[2]=90;
+     marks[3]=85;
+ System.out.println(marks[0]);
+System.out.println(marks[1]);
+System.out.println(marks[2]);
+System.out.println(marks[3]);
+}
+}
+    
