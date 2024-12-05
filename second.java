c6a6817c356078a1a56aa391f37ff8feee655234
Dipali lavate <120242897+dipali2003@users.noreply.github.com>
2024-12-05 15:31:22 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,11 @@
+public class second{
+public static void main(String args[])
+ {
+   int marks[]=new int[3];
+    marks[0]=34;
+    marks[1]=44;
+    marks[2]=54;
+    marks[3]=64;
+     System.out.print(marks[2]);
+}
+}
