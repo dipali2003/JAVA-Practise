65c4acba936104ad7388a921ce00cca77fa34f01
Dipali lavate <120242897+dipali2003@users.noreply.github.com>
2024-12-05 15:20:57 +0530

    Add files via upload

---
+++ lefttrigle.java
@@ -0,0 +1,18 @@
+class lefttrigle{
+public static void main(String args[])
+  {
+    int i,j,k;
+        for(i=4;i>=1;i--)
+         {
+           for(j=1;j<i;j++)
+            {
+             System.out.print(" ");
+             }
+               for(k=i;k<=4;k++)
+                 {
+                 System.out.print(" * ");
+                  }
+                     System.out.print("\n");
+            }
+      }
+}
